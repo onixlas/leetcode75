52909098060039c5f54cd1aaab8a010bc3e25006
Михаил Васильев <gnu.xinm@gmail.com>
2025-07-27 10:49:37 +0300

    Add tests

--- array-string/1768_merge_strings_alternately/solution.py
+++ array-string/1768_merge_strings_alternately/solution.py
@@ -9,7 +9,3 @@ class Solution:
                 result.append(word2[index])
 
         return ''.join(result)
-
-assert Solution().mergeAlternately(word1="abc", word2="pqr") == "apbqcr", "Test 1 Failed"
-assert Solution().mergeAlternately(word1="ab", word2="pqrs") == "apbqrs", "Test 2 Failed"
-assert Solution().mergeAlternately(word1="abcd", word2="pq") == "apbqcd", "Test 3 Failed"
---
+++ array-string/1768_merge_strings_alternately/test_solution.py
@@ -0,0 +1,11 @@
+import pytest
+from solution import Solution
+
+@pytest.mark.parametrize("word1, word2, expected", [
+    ("abc", "pqr", "apbqcr"),
+    ("ab", "pqrs", "apbqrs"),
+    ("abcd", "pq", "apbqcd")
+])
+def test_merge_alternately(word1, word2, expected):
+    solution = Solution()
+    assert solution.mergeAlternately(word1, word2) == expected
