ee61486fb15bb5845e7d192960371d3ff987fbfd
Михаил Васильев <gnu.xinm@gmail.com>
2025-07-29 13:33:09 +0300

    Add kids with candies

---
+++ array_string/kids_with_the_greatest_number_of_candies/solution.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        return [current_candies + extraCandies >= max_candies for current_candies in candies]
