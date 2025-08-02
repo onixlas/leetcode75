269e21c5f80aea79da130c1fdcbd1aba6244dd9b
Михаил Васильев <gnu.xinm@gmail.com>
2025-08-02 10:19:36 +0300

    add reverse vowels

---
+++ tests/test_move_zeroes.py
@@ -0,0 +1,15 @@
+import pytest
+from two_pointers.move_zeroes.solution import Solution
+
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([0, 1, 0, 3, 12], [1, 3, 12, 0, 0]),
+        ([0], [0]),
+        ([4, 2, 4, 0, 0, 3, 0, 5, 1, 0], [4, 2, 4, 3, 5, 1, 0, 0, 0, 0]),
+    ],
+)
+def test_move_zeroes(nums, expected):
+    Solution().moveZeroes(nums)
+    assert nums == expected
