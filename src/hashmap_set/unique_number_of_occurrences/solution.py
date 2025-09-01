7a9aa156b8049b22fd156bc0c20395401ced3e2f
Michael Vasiljev <gnu.xinm@gmail.com>
2025-09-01 10:57:11 +0300

    feat: [LeetCode #1207] Unique Number Of Occurrences (#10)
    Тип: hashmap/set
    Сложность: easy
    Временная сложность: O(n)
    Пространственная сложность: O(n)
    -
    
    Ссылка: https://leetcode.com/problems/unique-number-of-occurrences/

---
+++ src/hashmap_set/unique_number_of_occurrences/solution.py
@@ -0,0 +1,8 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counts = Counter(arr).values()
+        return len(counts) == len(set(counts))
